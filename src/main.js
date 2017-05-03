65acc9f3de8882ca4e10890e2c297fab84b44608
Nick Baroni <nick@rhythnic.com>
2017-05-03 07:54:05 -0400

    Added flow type checking to main.js.

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import over from 'ramda/src/over';
 import set from 'ramda/src/set';
 
 
-export const viewLenses = curry((enhancedLenses, state) => {
+export const viewLenses = curry((enhancedLenses: { }, state: {}) => {
   return Object.keys(enhancedLenses).reduce((acc, id) => {
     const val = enhancedLenses[id].view(state);
     if (typeof val !== 'undefined') acc[id] = val;
@@ -15,7 +15,9 @@ export const viewLenses = curry((enhancedLenses, state) => {
 });
 
 
-export const setStore = curry((enhancedLense, value) => {
+export const setStore = curry((
+  enhancedLense: { spec: { path: Array<string|number> }, lens: () => mixed },
+  value: any) => {
   return {
       path: enhancedLense.spec.path
     , lens: enhancedLense.lens
@@ -25,8 +27,9 @@ export const setStore = curry((enhancedLense, value) => {
 });
 
 
-export function lensReducer(reducer = identity) {
-  return (state = {}, action = {}) => {
+export function lensReducer(
+  reducer: (state: {}, action: { type: string, lens?: () => mixed, value?: any }) => {} = identity) {
+  return (state: {} = {}, action: { type: string, lens?: () => mixed, value?: any }): {} => {
     switch(action.type) {
     case 'SET_WITH_LENS':
       return lensSetter(state, action);
@@ -37,8 +40,9 @@ export function lensReducer(reducer = identity) {
 }
 
 
-export function lensSetter(state, { lens, value }) {
-  return typeof value === 'function'
-    ? over(lens, value, state)
-    : set(lens, value, state);
+export function lensSetter(state: {}, action: { lens?: () => mixed, value?: any }) {
+  if (!action.lens) return state;
+  return typeof action.value === 'function'
+    ? over(action.lens, action.value, state)
+    : set(action.lens, action.value, state);
 }
